d0bcbf613cea65e5c751686077feb7ce4c94d0cf
migli <migli0804.work@gmail.com>
2025-01-26 02:10:33 +0800

    added hero and projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+import Hero from "@/components/ui/Hero";
 import { Myprojects } from "@/components/ui/Projects";
 
 export default function Home() {
   return (
-    <div>
+    <main className=" pb-10 relative flex justify-center flex-col overflow-hidden mx-auto sm:px-10 bg-slate-900">
       helloooo
+      <Hero />
+      <div className="pt-10">
+          <div className="max-w-9xl w-full text-4xl text-white font-bold">
+            My Projects
+        </div>
+      </div>
       <Myprojects />
-    </div>
+    </main>
   );
 }
---
+++ components/ui/Hero.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Boxes } from './background-boxes'
+
+
+
+const Hero = () => {
+  return (
+    <div>
+        <div className="max-w-9xl mx-auto flex justify-center">
+        <div className="h-96 relative w-full max-w-9xl overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+          <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+          
+          <Boxes />
+          <h1 className="md:text-4xl text-xl text-center text-white relative z-20">
+            Miguel Antonio Li
+          </h1>
+          <p className="text-center mt-2 text-neutral-300 relative z-20">
+            Professional Data Analyst
+          </p>
+        </div>
+      </div>
+    </div>
+    
+
+  )
+}
+
+export default Hero
